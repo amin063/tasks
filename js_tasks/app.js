9872f2715d3e8ab10f98233e88510f8ae2bf95bb
amin063 <aminaliyev630@gmail.com>
2024-09-17 00:51:41 +0400

    Update

--- js_tasks/app.js
+++ js_tasks/app.js
@@ -1,182 +1,96 @@
-// // TASK 01
-// console.log("-------------- TASK 01 --------------");
+// TASK 01
+var num = +prompt("Reqem daxil edin");
 
-// console.log("My name is Amin");
+while(num <= 99 ) {
+    num += 7;
+    console.log(num);
+}
 
-// // TASK 02
-// console.log("-------------- TASK 02 --------------");
+// TASK 02
+var cycleNum = 0;
+var n = +prompt("N deyerini daxil edin");
+while(cycleNum < n) { 
+    cycleNum++;
+    console.log(`I know how to use cycles ${cycleNum}`);
+}
 
-// const userName = "Amin";
-// console.log(userName);
+// TASK 03
+var num = 100;
+while (num < 1000) {
+    console.log(num);
+    num += 10;
+}
 
-// // TASK 03
-// console.log("-------------- TASK 03 --------------");
+// TASK 04
+var num = 9;
+var sum = 0;
 
-// console.log("person, $add");
+while (num < 98) {
+    num += 2;
+    sum += num;
+}
+console.log(sum);
 
-// // TASK 04
-// console.log("-------------- TASK 04 --------------");
+// TASK 05
+var num = prompt("Reqem daxil edin")
+var bolunen = 99;
+var sum = 0;
+while (bolunen < 1000 ) {
+    bolunen++;
+    if(num % bolunen == 0) { // yanliz while istifade ederek yaza bilmedim
+        sum += bolunen
+    }
+}
+console.log(sum);
 
-// let bookPrice = 750;
-// let bookAmount = 14;
-// console.log(`Depodakı kitabların ümumi dəyəri: ${bookAmount * bookPrice}`);
+// TASK 06
 
+// TASK 07
+var num = 0;
+var hasil = 1;
 
+while (num < 10) {
+    num++;
+    hasil *= num;
+}
 
-// // TASK 05
-// console.log("-------------- TASK 05 --------------");
+console.log(hasil);
 
-// let kmh = 75;
-// let msSpeed = kmh * 1000 / 60;
-// console.log(msSpeed);
+// TASK 08
 
-// // TASK 06
-// console.log("-------------- TASK 06 --------------");
+var text = prompt("Metn daxil edin");
 
-// let userBirth = prompt("Doğum tarixiniz: (misal: 2001-09-11)");
-// const today = Date.now();
-// const birth = new Date(userBirth);
-// const gap = today - birth;
-// const dayGap = Math.floor(gap / 86400000);
-// console.log(`Bu fani dünyada ${dayGap} gün yaşayıbsan, ey fani!`);
+var textList = text.split(" ");
 
-// // TASK 07
-// console.log("-------------- TASK 07 --------------");
+var reverseText = textList.reverse();
 
-// let num = prompt("İkirəqəmli ədəd daxil edin: ")
-// const power = num**5
-// const relict = num%3
-// console.log(`${num} ^ 5 = ${power}\n${num} 3-ə bölünmədən alınan qalıq: ${relict}`);
+console.log(reverseText.join(" "));
 
+// TASK 09
 
-// // TASK 08
-// console.log("-------------- TASK 08 --------------");
-// var firstName = "Amin";
-// var lastName = "Əliyev";
-// console.log(`Sizin adınız ${firstName} ${lastName}. Tanışlığımıza çox şadam.`);
+var sentence = "Noqteni tapana hediyye."
 
-// // TASK 09
-// console.log("-------------- TASK 09 --------------");
-// console.log(`Sizi bir daha görməyimizə şadıq, ${firstName} ${lastName}`);
+var dotIndex = sentence.indexOf(".");
 
-// // TASK 10'
-// console.log("-------------- TASK 10 --------------");
+console.log(dotIndex);
 
-// let fullName = "Murad Orucov"
-// console.log(fullName.length - 1);
+// TASK 10
 
-// // TASK 11
-// console.log("-------------- TASK 11 --------------");
-
-// // let firstName = "Amin";
-// // let lastName = "Əliyev";
-// fullName = firstName + " " + lastName;
-// let firstIndexName = fullName.charAt(0);
-// let firstIndexLastName = fullName.charAt(5);
-// console.log(firstIndexName + " " +firstIndexLastName);
- 
-// // TASK 12
-// console.log("-------------- TASK 12 --------------");
-
-// fullName = prompt("Tam adınızı daxil edin: (AD SOYAD)");
-// let spaceIndex = fullName.indexOf(" ");
-// firstName = fullName.slice(0, spaceIndex);
-// lastName = fullName.slice(spaceIndex);
-// console.log(lastName + " " + firstName);
-
-// // TASK 13
-// console.log("-------------- TASK 13 --------------");
-// firstName = prompt("Adınızı daxil edin");
-// console.log(`Salam, ${firstName}`);
-
-// // TASK 14
-// console.log("-------------- TASK 14 --------------");
+var text = prompt("Metn daxil et");
+var textList = text.split("");
+var isNum = false;
 
 
-// let firstPrompt = prompt("Birinci rəqəmi daxil edin");
-// let secondPrompt = prompt("İkinci rəqəmi daxil edin");
+for (i = 0; i < textList.length; i++) {
+    if (!isNaN(textList[i])) {
+        isNum = true;       
+    }
+}
 
-// let firstNumber = Number(firstPrompt);
-// let secondNumber = Number(secondPrompt);
-
-// let sum = firstNumber + secondNumber;
-// let subs = firstNumber - secondNumber;
-// let sec = firstNumber * secondNumber;
-// let mult = firstNumber / secondNumber;
-
-// console.log(`Toplama: ${sum}\nÇıxma: ${subs}\nVurma: ${sec}\nBölmə: ${mult}`);
-
-// // TASK 15
-// console.log("-------------- TASK 15 --------------");
-
-// firstNumber = 12345
-// secondNumber = 98765
-// num = firstNumber + secondNumber;
-// lastIndexNum = num.toString().charAt(5);
-// console.log(lastIndexNum);
-
-
-
-
-// TASK 17
-console.log("-------------- TASK 17 --------------");
-var userNum = prompt("4 rəqəmli ədəd daxil edin");
-var digitNum = +userNum[0] + +userNum[1] + +userNum[2] + +userNum[3];
-console.log(digitNum);
-
-// TASK 18
-console.log("-------------- TASK 18 --------------");
-
-var firstName = prompt("Adınızı daxil edin");
-console.log(`Salam, ${firstName}`);
-
-// TASK 19
-console.log("-------------- TASK 19 --------------");
-
-var userPrice = prompt("«530.90» formatında qiymət daxil edin");
-var dotIndex = userPrice.indexOf(".") 
-var manat = userPrice.slice(0, dotIndex);
-var qep = userPrice.slice(dotIndex + 1);
-console.log(`${manat} manat ${qep} qəpik`);
- 
-// TASK 20
-console.log("-------------- TASK 20 --------------");
-
-var userSalary = prompt("Əmək haqqınızı daxil edin");
-var taxSalary = +userSalary * (100 - 13) / 100;
-console.log(taxSalary);
- 
-// TASK 21
-console.log("-------------- TASK 21 --------------");
-
-var firstName = prompt("Zəhmət olmasa adınız daxil edin");
-var lastName = prompt("Zəhmət olmasa soyadınız daxil edin");
-var fatherName = prompt("Zəhmət olmasa ata adınız daxil edin");
-console.log(`${firstName} ${lastName} ${fatherName}`);
-
-// TASK 22
-console.log("-------------- TASK 22 --------------");
-
-var amount = prompt("Zəhmət olmasa əmanətin məbləğini daxil edin");
-var time = prompt("Zəhmət olmasa əmanətin müddətini daxil edin");
-var rate = prompt("Zəhmət olmasa əmanətin faiz dərəcəsini adınız daxil edin");
-
-var profit = amount * time * rate;
-
-console.log(Number(profit) / 100);
-
-// TASK 23
-console.log("-------------- TASK 23 --------------");
-var value = 10;
-
-console.log(value == 10);
-console.log(value == 20);
-console.log(value != 10);
-console.log(value!=20);
-console.log(value < 15);
-
-
-
-
-
+if(isNum) {
+    console.log("Reqem daxil etmisen");
+}else {
+    console.log("Reqem daxil etmemisen");
+    
+}
 
