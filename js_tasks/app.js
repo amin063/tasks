9d32be209c0e7a6d338fe1c1ab2edd940b1b572e
amin063 <aminaliyev630@gmail.com>
2024-09-18 23:42:54 +0400

    Update

--- js_tasks/app.js
+++ js_tasks/app.js
@@ -1,7 +1,7 @@
 // TASK 01
 var num = +prompt("Reqem daxil edin");
 
-while(num <= 99 ) {
+while (num <= 97) {
     num += 7;
     console.log(num);
 }
@@ -9,7 +9,7 @@ while(num <= 99 ) {
 // TASK 02
 var cycleNum = 0;
 var n = +prompt("N deyerini daxil edin");
-while(cycleNum < n) { 
+while (cycleNum < n) {
     cycleNum++;
     console.log(`I know how to use cycles ${cycleNum}`);
 }
@@ -32,18 +32,50 @@ while (num < 98) {
 console.log(sum);
 
 // TASK 05
-var num = prompt("Reqem daxil edin")
-var bolunen = 99;
+
+var num = +prompt("Reqem daxil edin")
+var bolunen = num;
 var sum = 0;
-while (bolunen < 1000 ) {
-    bolunen++;
-    if(num % bolunen == 0) { // yanliz while istifade ederek yaza bilmedim
+
+while (bolunen < 1000 && bolunen % num === 0) {
+    bolunen += num;
+    if (bolunen > 100 && bolunen < 1000) {
         sum += bolunen
     }
 }
 console.log(sum);
 
 // TASK 06
+var num = prompt("Eded daxil edin");
+var count = 0;
+while (num > 0) {
+    count = 0;
+    for (i = num; i > 0; i--) {
+        if (num % i === 0) {
+            count++;
+        }
+        if (i === 1) {
+            if (count < 3) {
+                console.log(`${num} sade ededdir`);
+            }
+        }
+    }
+    num--;
+}
+
+// TASK 07
+var userNum = prompt("Neceye qeder Fibonacci ededleri gormek isteyirsen");
+var num = 1;
+var firstNum = 1;
+var sum = 1;
+while (sum < userNum) {
+    console.log(sum);
+    firstNum = num;
+    num = sum;
+    sum = num + firstNum;
+    userNum = sum;
+}
+
 
 // TASK 07
 var num = 0;
@@ -83,14 +115,14 @@ var isNum = false;
 
 for (i = 0; i < textList.length; i++) {
     if (!isNaN(textList[i])) {
-        isNum = true;       
+        isNum = true;
     }
 }
 
-if(isNum) {
+if (isNum) {
     console.log("Reqem daxil etmisen");
-}else {
+} else {
     console.log("Reqem daxil etmemisen");
-    
+
 }
 
